e511725135a77d34f4272559d040c43b43fcbfd5
Lala2398 <lala.ibadullayeva@gmail.com>
2025-02-26 15:29:29 +0000

    5-print_numbers.c

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -1,15 +1,18 @@
-#include <studio.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
 int main(void)
 {
-	char only ="0";
-	while (only <="9")
-	{
-		putchar(only);
-		only++;
-	}
-	putchar ('\n');
-	return (0);
+    char only = '0';
+
+    while (only <= '9')
+    {
+        putchar(only);
+        only++;
+    }
+    putchar('\n');
+
+    return (0);
 }
+
