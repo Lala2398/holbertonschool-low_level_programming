20cec9a869b1cef3b8055f46db36a7a1c6dd8ccb
Lala2398 <lala.ibadullayeva@gmail.com>
2025-02-28 12:48:42 +0000

    8-print_base16.c

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -1,24 +1,16 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
-/**
- * main - Entry point
- * Return: Always 0 (Success)
- */
+
 int main(void)
 {
-	int a = 48;
-	while (a != 103)
+	int i;
+
+	for (i = 0; i < 16; i++)
 	{
-		if (a > 57 && a < 97)
-		{
-			a++;
-			continue;
-		}
-		putchar(a);
-		a++;
+		if (i < 10)
+			putchar(i + 48);
+		else
+			putchar(i + 87);
 	}
 	putchar('\n');
 	return (0);
-}
-~              
+}       
