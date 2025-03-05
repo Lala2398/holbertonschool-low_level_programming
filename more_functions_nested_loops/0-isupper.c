8adc449bf077098efe96cbb8736b9ded43bc0563
lala2398 <lala.ibadullayeva@gmail.com>
2025-03-05 14:48:22 +0000

    0-isupper.c

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -3,17 +3,16 @@
 /**
  * _isupper - checks if a character is uppercase
  * @a: the character to be checked
- * 
  * Return: 1 if character is uppercase, 0 otherwise
  */
 int _isupper(int a)
 {
-    if (a >= 'A' && a <= 'Z')
-    {
-        return (1);
-    }
-    else
-    {
-        return (0);
-    }
+	if (a >= 'A' && a <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
