1d615dd42ee44ec29d306b277b210f56eaccbe8d
lala2398 <lala.ibadullayeva@gmail.com>
2025-04-07 14:21:18 +0000

    1-args.c

---
+++ argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - check the code
+ * @argc: number of argv
+ * @argv: vector
+ * Return: Always 0.
+ */
+int main(int argc, char* argv[])
+{
+	int i, count =0 ;
+    for ( i=1 ; i< argc; i++)
+    count++;
+	printf("%d\n", count);
+	return (0);
+}
