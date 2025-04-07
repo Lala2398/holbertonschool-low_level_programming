8afec81246edfe1e115db6ddb7ff9109365df6ed
lala2398 <lala.ibadullayeva@gmail.com>
2025-04-07 14:26:08 +0000

    1-args.c

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -6,11 +6,12 @@
  * @argv: vector
  * Return: Always 0.
  */
-int main(int argc, char* argv[])
+int main(int argc, char **argv)
 {
-        int i, count =0 ;
+	(void) argv;
+    int i, count =0 ;
     for ( i=1 ; i< argc; i++)
     count++;
-        printf("%d\n", count);
-        return (0);
+	printf("%d\n", count);
+	return (0);
 }
